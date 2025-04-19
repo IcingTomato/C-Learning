6829ba16112ca53e61d71ef9f49d83f21665ba9c
Icing Tomato <24474531+IcingTomato@users.noreply.github.com>
2025-04-19 22:19:26 +0800

    Add: Added 0.assembly.c

--- 1.helloworld.c
+++ 1.helloworld.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "shit.h"
 
 int main(void)
 {
--- cls_exe.ps1
+++ cls_exe.ps1
@@ -1,12 +1,19 @@
+param(
+    [string]$gitcommit = "Change: Changed several files"
+)
+
 # Get the directory where the current script is located
 $CurrentDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # Get all .exe files in the same directory
 $ExeFiles = Get-ChildItem -Path $CurrentDirectory -Filter *.exe
 
-if ($ExeFiles.Count -eq 0) {
+if ($ExeFiles.Count -eq 0) 
+{
     Write-Host "No .exe files found."
-} else {
+} 
+
+else {
     Write-Host "The following .exe files will be deleted:"
     $ExeFiles | ForEach-Object {
         Write-Host $_.FullName
@@ -17,3 +24,8 @@ if ($ExeFiles.Count -eq 0) {
 
     Write-Host "Cleanup completed!"
 }
+
+# Stage all changes, commit, and push to Git
+git add .
+git commit -m $gitcommit
+git push
