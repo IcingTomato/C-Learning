78a52c1c5f07911cddb3e82cf51d3bf033c92ca5
Icing Tomato <24474531+IcingTomato@users.noreply.github.com>
2025-04-21 11:00:40 +0800

    Change: Changed C/12.notEqual.c, README.md

---
+++ C/12.notEqual.c
@@ -0,0 +1,17 @@
+#include "shit.h"
+
+int main(void)
+{
+    int n1, n2;
+
+    puts("Enter 2 integers:");
+    printf("Integer 1:"); scanf("%d", &n1);
+    printf("Integer 2:"); scanf("%d", &n2);
+
+    if (n1 != n2)
+        puts("Not equal.");
+    else
+        puts("Equal.");
+
+    return 0;
+}
