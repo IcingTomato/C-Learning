92041c4cf866de8d104537e21359dd37df9af2b0
Icing Tomato <24474531+IcingTomato@users.noreply.github.com>
2025-04-26 01:22:34 +0800

    Change: Changed C/21.sumUp/

---
+++ C/21.sumUp/21.sumUp.c
@@ -0,0 +1,23 @@
+#include "../shit.h"
+
+int main(void)
+{
+    int sum = 0;
+    int cnt = 0;
+    int retry;
+
+    do
+    {
+        int t;
+
+        printf("Enter an integer:"); scanf("%d", &t);
+        sum = sum + t;
+        cnt = cnt + 1;
+
+        printf("Continue? [Yes...0/No...9]:"); scanf("%d", &retry);
+    } while (retry == 0);
+
+    printf("Sum is %d, average is %.2f.\n", sum, (double)sum / cnt);
+
+    return 0;
+}
