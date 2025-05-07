52fa54ec8b14732a2cf3f6bcf4829065a647436a
Icing Tomato <24474531+IcingTomato@users.noreply.github.com>
2025-05-07 22:47:14 +0800

    Change: Changed C/41.triangle/triangle3.c

---
+++ C/41.triangle/triangle3.c
@@ -0,0 +1,21 @@
+#include "../shit.h"
+
+int main(void)
+{
+	int i, j, len;
+
+	puts("Generates an isosceles right-angled triangle with a right angle at the bottom right.");
+
+	printf("Short side:"); scanf("%d", &len);
+
+	for (i = 1; i <= len; i++)
+	{
+		for (j = 1; j <= len - i; j++)
+			putchar(' ');
+		for (j = 1; j <= i; j++)
+			putchar('*');
+		putchar('\n');
+	}
+
+	return 0;
+}
