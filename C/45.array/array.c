220ee40b026b8c25fcce1b494ca58fb4a1a78477
Icing Tomato <24474531+IcingTomato@users.noreply.github.com>
2025-05-16 10:54:42 +0800

    Change: Changed C/45.array/array.c

---
+++ C/45.array/array.c
@@ -0,0 +1,17 @@
+#include "../shit.h"
+
+int main(void)
+{
+	int i;
+	int v[5];
+
+	for (i = 0; i < 5; i++)
+		v[i] = i + 1;
+
+	// 遍历 traverse
+
+	for (i = 0; i < 5; i++)
+		printf("v[%d] = %d\n", i, v[i]);
+
+	return 0;
+}
